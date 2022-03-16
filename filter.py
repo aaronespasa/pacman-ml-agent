c8daa46cb0fc7f35a9151873e720941e707868eb
aaronespasa <aaespasa@gmail.com>
2022-03-16 17:07:49 +0100

    Base code + Testing data

---
+++ filter.py
@@ -0,0 +1,11 @@
+import arff
+
+f1 = open('filter_data_pacman_manual1.arff', "a")
+f2 = open('filter_data_pacman_manual2.arff', "a")
+
+for row in arff.load('all_data_pacman.arff'):
+    f1.write(f"{row.pacmanX},{row.pacmanY},{row.ghostX},{row.ghostY},{row.directionTaken}\n")
+    f2.write(f"{row.pacmanX},{row.pacmanY},{row.directionPossible},{row.ghostX},{row.ghostY},{row.nearestGhostDistance},{row.score},{row.directionTaken}\n")
+
+f1.close()
+f2.close()
